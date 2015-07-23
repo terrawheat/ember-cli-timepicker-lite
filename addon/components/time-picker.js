81f24a8ab9b167c9f8e21e2f696d6fe961625c52
Matt Wheatley <terrawheat@gmail.com>
2015-07-23 19:38:39 +0100

    Added ability to customise exactly which fields show

--- addon/components/time-picker.js
+++ addon/components/time-picker.js
@@ -4,5 +4,15 @@ import layout from '../templates/components/time-picker';
 export default Ember.Component.extend({
   layout: layout,
   tagName: 'span',
-  classNames: ['timepicker']
+  classNames: ['timepicker'],
+
+  in24hr: true,
+
+  showHours: true,
+  showMinutes: true,
+  showSeconds: false,
+  showTimeZone: false,
+  showMeridian: Ember.computed('in24hr', function () {
+    return this.get('in24hr') === false;
+  })
 });
