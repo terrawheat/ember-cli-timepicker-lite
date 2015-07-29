2d53a1f969b1baba39bf37fa5491f044efe3a0f0
Matt Wheatley <matthew.wheatley@heg.com>
2015-07-29 13:08:09 +0100

    Improved default styling and better focus spawning behaviour

--- addon/components/time-picker.js
+++ addon/components/time-picker.js
@@ -6,6 +6,9 @@ export default Ember.Component.extend({
   tagName: 'span',
   classNames: ['timepicker'],
 
+  show: false,
+  eventNamespace: 'timepicker',
+
   hours: '12',
   minutes: '58',
   seconds: '00',
@@ -30,6 +33,41 @@ export default Ember.Component.extend({
     seconds: { min: 0, max: 59 },
   },
 
+  direction: 'w',
+
+  didInsertElement: function () {
+    var ns = this.get('eventNamespace');
+    var that = this;
+
+    Ember.$('#upgrade-app').on(`click.${ns}`, function (e) {
+      var $target = Ember.$(e.target);
+      var inPicker = false;
+      var shown = that.get('show');
+
+      if ($target.closest('.timepicker').length > 0) {
+        inPicker = true;
+      }
+
+      if (inPicker === false) {
+        if (shown) {
+          that.set('show', false);
+        }
+
+        return true;
+      }
+
+      if (shown === false) {
+        that.set('show', true);
+      }
+    });
+  },
+
+  willDestroyElement: function () {
+    var ns = this.get('eventNamespace');
+
+    Ember.$('#upgrade-app').off(`click.${ns}`);
+  },
+
   actions: {
     increase: function (field) {
       this.get('increase').apply(this, [field, this.get(`${field}Interval`)]);
@@ -48,7 +86,7 @@ export default Ember.Component.extend({
     if (value > limit.max) {
       value = limit.min;
     }
-
+    value = this.setLeadingZero(value);
     this.set(field, value);
   },
 
@@ -61,7 +99,17 @@ export default Ember.Component.extend({
     if (value < limit.min) {
       value = limit.max;
     }
-
+    value = this.setLeadingZero(value);
     this.set(field, value);
+  },
+
+  setLeadingZero: function (value) {
+    var val = String(value);
+
+    if (Number(value) < 10) {
+      val = '0' + val;
+    }
+
+    return val;
   }
 });
